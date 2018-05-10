54bb067678b64ffeffa591d3e578b57092fd5b81
blackbrs <guillermobrs@gmail.com>
2018-05-09 18:22:57 -0600

    Initial commit

---
+++ app/src/main/java/creator/test/logintest/MainActivity.java
@@ -0,0 +1,14 @@
+package creator.test.logintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+    }
+}
