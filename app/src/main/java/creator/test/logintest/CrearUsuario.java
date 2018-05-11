5a029bd5e769a1a2cb15676f027163833675f6ae
blackbrs <guillermobrs@gmail.com>
2018-05-10 21:00:04 -0600

    version 1

---
+++ app/src/main/java/creator/test/logintest/CrearUsuario.java
@@ -0,0 +1,17 @@
+package creator.test.logintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CrearUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_crear_usuario);
+    }
+
+
+}
--- app/src/main/java/creator/test/logintest/MainActivity.java
+++ app/src/main/java/creator/test/logintest/MainActivity.java
@@ -1,14 +1,30 @@
 package creator.test.logintest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity implements   View.OnClickListener{
 
+private Button btncusuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        btncusuario = (Button) findViewById(R.id.registrar);
     }
+
+    public void onClick(View v){
+
+        int id = v.getId();
+        if(id == btncusuario.getId()){
+            Intent intent = new Intent(this, CrearUsuario.class);
+            startActivity(intent);
+        }
+
+}
+
 }
