dbe9bce9389eb01f7da1885f6a195b1912c6cdcb
blackbrs <guillermobrs@gmail.com>
2018-05-12 15:30:38 -0600

    Mejoras y gradients XML animacion de splash de inicio mejor fluides

--- app/src/main/java/creator/test/logintest/AreaEvaluacionInsertarActivity.java
+++ app/src/main/java/creator/test/logintest/AreaEvaluacionInsertarActivity.java
@@ -8,6 +8,7 @@ public class AreaEvaluacionInsertarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_area_evaluacion_insertar);
     }
 }
