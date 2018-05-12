6b6807e8c094e675a5080ce0d31eac7754b07b61
Mego1596 <maurigranados1596@gmail.com>
2018-05-12 13:10:52 -0600

    VISTAS DE GESTION DE DOCENTE

--- app/src/main/java/creator/test/logintest/MenuDocenteActivity.java
+++ app/src/main/java/creator/test/logintest/MenuDocenteActivity.java
@@ -1,13 +1,38 @@
 package creator.test.logintest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+//import android.content.Intent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+//import android.view.View;
+//import android.widget.ListView;
 
-public class MenuDocenteActivity extends AppCompatActivity {
+public class MenuDocenteActivity extends ListActivity {
 
+    String[] menu={"Gestionar Area de Evaluacion","Gestionar Cuestionario","Consultar Oferta Academica"};
+    String[] activities={"AreaEvaluacionMenuActivity", "CuestionarioMenuActivity","OfertaAcademicaConsultaActivity"};
+    ControlBDHelper BDHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_docente);
+        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu));
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l,v,position,id);
+
+        String nombreValue = activities[position];
+        try{
+            Class<?> clase = Class.forName("creator.test.logintest." + nombreValue);
+            Intent inte = new Intent(this,clase);
+            startActivity(inte);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
