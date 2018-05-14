12d5c24681442ab806fa7bef684799d7179d2c93
Mego1596 <maurigranados1596@gmail.com>
2018-05-14 10:57:36 -0600

    Consultar Pregunta

--- app/src/main/java/creator/test/logintest/PreguntaConsultarActivity.java
+++ app/src/main/java/creator/test/logintest/PreguntaConsultarActivity.java
@@ -7,25 +7,30 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class PreguntaConsultarActivity extends AppCompatActivity {
-
+    ListView listaPreguntas;
     Spinner comboArea;
     ControlBDHelper helper;
     ArrayList<String> listaArea;
     ArrayList<AreaEvaluacion> AreasList;
+    ArrayList<String> listaPregunta;
+    ArrayList<Pregunta> PreguntasList;
     EditText descripcion;
     int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         helper = new ControlBDHelper(this);
-        setContentView(R.layout.activity_pregunta_eliminar);
+        setContentView(R.layout.activity_pregunta_consultar);
         comboArea = (Spinner) findViewById(R.id.spinnerArea);
         descripcion = (EditText) findViewById(R.id.txtDescrip);
+        listaPreguntas = (ListView) findViewById(R.id.listViewPregunta);
         getSupportActionBar().hide();
         consultarListaArea();
         ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,listaArea);
@@ -48,6 +53,35 @@ public class PreguntaConsultarActivity extends AppCompatActivity {
 
 
     }
+    private void consultarPreguntas(){
+
+           Pregunta preg1l;
+           PreguntasList = new ArrayList<Pregunta>();
+           helper.abrir();
+           helper.cerrar();
+           helper.abrir();
+           Cursor cursorPregunta= helper.consultarListaPregunta(id);
+           while(cursorPregunta.moveToNext()){
+               preg1l = new Pregunta();
+               preg1l.setDescrippreg(cursorPregunta.getString(0));
+               PreguntasList.add(preg1l);
+           }
+           obtenerListaPreguntas();
+    }
+
+    private void obtenerListaPreguntas() {
+        listaPregunta = new ArrayList<String>();
+        for (int i=0; i < PreguntasList.size(); i++){
+        listaPregunta.add(PreguntasList.get(i).getDescrippreg());
+    }
+}
+
+    public void consultarListaPreguntas(View v){
+        consultarPreguntas();
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,listaPregunta);
+        listaPreguntas.setAdapter(adapter);
+    }
+
 
     private void consultarListaArea() {
         AreaEvaluacion area1;
@@ -76,3 +110,4 @@ public class PreguntaConsultarActivity extends AppCompatActivity {
     }
 
 }
+
