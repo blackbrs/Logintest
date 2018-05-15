8115010ecdb9be42db7aced323048a808c93db1e
blackbrs <guillermobrs@gmail.com>
2018-05-15 10:38:45 -0600

    MAMAMA MAURI

--- app/src/main/java/creator/test/logintest/CuestionarioEliminarActivity.java
+++ app/src/main/java/creator/test/logintest/CuestionarioEliminarActivity.java
@@ -7,6 +7,7 @@ public class CuestionarioEliminarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuestionario_eliminar);
     }
